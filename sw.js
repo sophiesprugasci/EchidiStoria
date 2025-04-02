9c00702886c4557454910d363c56576d8c92cc23
sophie <118370459+sophiesprugasci@users.noreply.github.com>
2025-04-02 13:48:44 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,18 @@
-self.addEventListener("install", function(event) {
-  console.log("Service Worker installed");
+//self.addEventListener("install", function(event) {
+ // console.log("Service Worker installed");
+//});
+
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
 });
